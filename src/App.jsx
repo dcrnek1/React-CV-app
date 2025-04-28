191ae88695e290a18c8d42b1d59001c2f2703589
dcrnek1 <dario.crnek@gmail.com>
2025-04-28 15:48:36 +0200

    Added tailwindcss and lucide-react icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
+import {BatteryWarning} from 'lucide-react'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      
+      <h1>Test</h1>
+      <p>Test</p>
+      <button className="btn btn-blue">Test</button>
+      <input type="text" placeholder='Test' className="btn btn-blue" />
+      <BatteryWarning />
     </>
   )
 }
