d92a455ed7912b16a860353f7f24e00ce731832c
dcrnek1 <dario.crnek@gmail.com>
2025-04-29 14:54:48 +0200

    responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import PreviewCV from "./components/views/PreviewCS";
 
 function App() {
   return (
-    <div className="flex flex-col min-h-full items-center">
+    <div className="flex flex-col min-h-screen items-center">
       <Header />
-      <div className="flex flex-col lg:flex-row flex-1 w-full lg:max-w-[1440px]">
+      <div className="flex flex-col lg:flex-row flex-1 w-full items-stretch lg:max-w-[1440px]">
         <EditCV />
         <PreviewCV />
       </div>
